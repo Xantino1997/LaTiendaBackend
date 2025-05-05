c3bafcfd9e3b3163978d761273a2af287e8ca866
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2025-05-05 09:48:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,79 +1,69 @@
-const express = require('express')
-const app = express()
+// // ✅ index.js
+require("dotenv").config();
+const express = require("express");
+const connectDB = require("./config/db");
+const cors = require("cors");
+const path = require("path");
 
-app.use(express.json())
-app.get('/test',(req,res)=>{
-  res.json({msg:"hola vercel como estas?"})})
-const PORT = 3000
+const app = express();
+const fs = require("fs");
 
-app.listen(PORT,()=>{
-  console.log(`hola puerto ${PORT}`);})
+// Conectar a MongoDB
+connectDB();
+const uploadsDir = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadsDir)) {
+  fs.mkdirSync(uploadsDir);
+}
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+app.use("/uploads", express.static("uploads")); // para servir imágenes
+
+// Ruta de prueba
+app.get("/test", (req, res) => {
+  res.send("Hola Vercel");
+});
+
+// Rutas
+const mercadoPagoRoutes = require("./router/mercadoPagoRoutes");
+app.use("/api/mercadopago", mercadoPagoRoutes);
+
+const viumiRoutes = require("./router/viumiWallet");
+app.use("/api/viumi", viumiRoutes);
+
+const proveedorRoutes = require("./router/proveedorRoutes");
+app.use("/api/proveedor", proveedorRoutes);
+
+const compraCreditoRoutes = require("./router/compraCreditoRoutes");
+app.use("/api/compra-credito", compraCreditoRoutes);
+
+const clienteRoutes = require("./router/clienteRoutes");
+app.use("/api/cliente", clienteRoutes);  // Solo clientes
+
+const compraRoutes = require("./router/compra");
+app.use("/api/compra", compraRoutes);   // Solo compras generales
+
+app.use("/api/products", require("./router/productRoutes"));
+
+const authRoutes = require("./router/authRoutes");
+app.use("/api/auth", authRoutes);
+
+// Para servir archivos estáticos (como imágenes subidas)
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Ruta de carga de imágenes
+const uploadRoute = require("./router/upload");
+app.use("/api/upload", uploadRoute);
+
+// Agregar app.listen para entorno local
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
+
 
 module.exports = app
-// // ✅ index.js
-// require("dotenv").config();
-// const express = require("express");
-// const connectDB = require("./config/db");
-// const cors = require("cors");
-// const path = require("path");
-
-// const app = express();
-// const fs = require("fs");
-
-// // Conectar a MongoDB
-// connectDB();
-// const uploadsDir = path.join(__dirname, "uploads");
-// if (!fs.existsSync(uploadsDir)) {
-//   fs.mkdirSync(uploadsDir);
-// }
-
-// // Middlewares
-// app.use(cors());
-// app.use(express.json());
-// app.use("/uploads", express.static("uploads")); // para servir imágenes
-
-// // Ruta de prueba
-// app.get("/test", (req, res) => {
-//   res.send("Hola Vercel");
-// });
-
-// // Rutas
-// const mercadoPagoRoutes = require("./router/mercadoPagoRoutes");
-// app.use("/api/mercadopago", mercadoPagoRoutes);
-
-// const viumiRoutes = require("./router/viumiWallet");
-// app.use("/api/viumi", viumiRoutes);
-
-// const proveedorRoutes = require("./router/proveedorRoutes");
-// app.use("/api/proveedor", proveedorRoutes);
-
-// const compraCreditoRoutes = require("./router/compraCreditoRoutes");
-// app.use("/api/compra-credito", compraCreditoRoutes);
-
-// const clienteRoutes = require("./router/clienteRoutes");
-// app.use("/api/cliente", clienteRoutes);  // Solo clientes
-
-// const compraRoutes = require("./router/compra");
-// app.use("/api/compra", compraRoutes);   // Solo compras generales
-
-// app.use("/api/products", require("./router/productRoutes"));
-
-// const authRoutes = require("./router/authRoutes");
-// app.use("/api/auth", authRoutes);
-
-// // Para servir archivos estáticos (como imágenes subidas)
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-// // Ruta de carga de imágenes
-// const uploadRoute = require("./router/upload");
-// app.use("/api/upload", uploadRoute);
-
-// // Agregar app.listen para entorno local
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => {
-//   console.log(`Servidor corriendo en http://localhost:${PORT}`);
-// });
-
 
 
 
