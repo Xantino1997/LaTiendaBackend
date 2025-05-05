cd9e9455d6bdd9c1860da9c38091648f0b1588d3
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2025-05-05 09:06:19 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// api/index.js
+// ✅ index.js
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
@@ -6,17 +6,23 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const fs = require("fs");
+const PORT = process.env.PORT || 5000;
 
+// Conectar a MongoDB
 connectDB();
-const uploadsDir = path.join(__dirname, "uploads");
+const fs = require("fs");
+app.use("/uploads", express.static("uploads"));
+
+// Crear carpeta uploads si no existe
+const uploadsDir = path.join(\_\_dirname, "uploads");
 if (!fs.existsSync(uploadsDir)) {
-  fs.mkdirSync(uploadsDir);
+fs.mkdirSync(uploadsDir);
 }
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads")); // Servir imágenes
+app.use("./uploads", express.static(path.join(\_\_dirname, "uploads"))); // para servir imágenes
 
 // Rutas
 const mercadoPagoRoutes = require("./router/mercadoPagoRoutes");
@@ -32,21 +38,27 @@ const compraCreditoRoutes = require("./router/compraCreditoRoutes");
 app.use("/api/compra-credito", compraCreditoRoutes);
 
 const clienteRoutes = require("./router/clienteRoutes");
-app.use("/api/cliente", clienteRoutes);
+app.use("/api/cliente", clienteRoutes);  // Solo clientes
 
 const compraRoutes = require("./router/compra");
-app.use("/api/compra", compraRoutes);
+app.use("/api/compra", compraRoutes);   // Solo compras generales
 
 app.use("/api/products", require("./router/productRoutes"));
+
 const authRoutes = require("./router/authRoutes");
+
 app.use("/api/auth", authRoutes);
 
+// Para servir archivos estáticos (como imágenes subidas)
+app.use("./uploads", express.static(path.join(\_\_dirname, "uploads")));
+
+// Ruta de carga de imágenes
 const uploadRoute = require("./router/upload");
 app.use("/api/upload", uploadRoute);
 
-// Exportar la función para Vercel
-module.exports = app;
-
+app.listen(PORT, () => {
+console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
 
 
 
