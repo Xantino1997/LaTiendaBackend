55ae9e153bd673bb1d87f5bbd24cb1012c2597ad
Xantino1997 <106495276+Xantino1997@users.noreply.github.com>
2025-05-05 08:58:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-
-// ✅ index.js
+// api/index.js
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./config/db");
@@ -7,32 +6,24 @@ const cors = require("cors");
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
-
-// Conectar a MongoDB
-connectDB();
 const fs = require("fs");
-app.use("/uploads", express.static("uploads"));
 
-// Crear carpeta uploads si no existe
+connectDB();
 const uploadsDir = path.join(__dirname, "uploads");
 if (!fs.existsSync(uploadsDir)) {
   fs.mkdirSync(uploadsDir);
 }
 
-// Middlewares
 app.use(cors());
 app.use(express.json());
-app.use("./uploads", express.static(path.join(__dirname, "uploads"))); // para servir imágenes
+app.use("/uploads", express.static("uploads")); // Servir imágenes
 
 // Rutas
 const mercadoPagoRoutes = require("./router/mercadoPagoRoutes");
 app.use("/api/mercadopago", mercadoPagoRoutes);
 
-
 const viumiRoutes = require("./router/viumiWallet");
 app.use("/api/viumi", viumiRoutes);
-
 
 const proveedorRoutes = require("./router/proveedorRoutes");
 app.use("/api/proveedor", proveedorRoutes);
@@ -41,30 +32,20 @@ const compraCreditoRoutes = require("./router/compraCreditoRoutes");
 app.use("/api/compra-credito", compraCreditoRoutes);
 
 const clienteRoutes = require("./router/clienteRoutes");
-app.use("/api/cliente", clienteRoutes);  // Solo clientes
+app.use("/api/cliente", clienteRoutes);
 
 const compraRoutes = require("./router/compra");
-app.use("/api/compra", compraRoutes);   // Solo compras generales
+app.use("/api/compra", compraRoutes);
 
 app.use("/api/products", require("./router/productRoutes"));
-
-
-const authRoutes = require("./router/authRoutes"); 
-
+const authRoutes = require("./router/authRoutes");
 app.use("/api/auth", authRoutes);
 
-
-// Para servir archivos estáticos (como imágenes subidas)
-app.use("./uploads", express.static(path.join(__dirname, "uploads")));
-
-// Ruta de carga de imágenes
 const uploadRoute = require("./router/upload");
 app.use("/api/upload", uploadRoute);
 
-
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en http://localhost:${PORT}`);
-});
+// Exportar la función para Vercel
+module.exports = app;
 
 
 
